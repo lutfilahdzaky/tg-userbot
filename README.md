748467ace99f9fb1d2fc68cdd03faca759d75e51
Lutfilah Dzaky <lutfilahdzaky12@gmail.com>
2020-08-26 21:34:51 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Deploy Now
+
+<p align="left"><a href="https://heroku.com/deploy?template=https://github.com/lutfilahdzaky/tg-userbot/tree/sql-extended"> <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy to Heroku" /></a></p>
---
+++ userbot/modules/p.py
@@ -0,0 +1,22 @@
+from time import sleep
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^p(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("Biasakanlah Mengucapkan Salam")
+    sleep(1)
+    await typew.edit("Assalamualaikum")
+# Create by myself @rizkynfs
+
+
+@register(outgoing=True, pattern='^P(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("Biasakanlah Mengucapkan Salam")
+    sleep(1)
+    await typew.edit("Assalamualaikum")
+# Create by myself @rizkynfs
